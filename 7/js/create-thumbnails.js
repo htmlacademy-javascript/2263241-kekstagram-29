9936818507813b5898c62c7d924cd46d7af5cbf5
Keks <keks@htmlacademy.ru>
2023-07-07 12:54:15 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/create-thumbnails.js
@@ -0,0 +1,29 @@
+//import {createGallery} from './create-gallery.js';
+const createThumbnails = (picturesArray) => {
+
+  const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const picturesContainer = document.querySelector('.pictures');
+
+  const picturestFragment = document.createDocumentFragment();
+
+  //const picturesGallery = createGallery();
+
+  picturesArray.forEach(({id, url, description,comments, likes}) => {
+    const picturesElement = picturesTemplate.cloneNode(true);
+    const pictureElementImg = picturesElement.querySelector('.picture__img');
+
+    pictureElementImg.id = id;
+    pictureElementImg.src = url;
+    pictureElementImg.alt = description;
+
+    picturesElement.querySelector('.picture__comments').textContent = comments.length;
+    picturesElement.querySelector('.picture__likes').textContent = likes;
+
+    picturestFragment.append(picturesElement);
+  });
+
+  picturesContainer.append(picturestFragment);
+
+};
+
+export {createThumbnails};
